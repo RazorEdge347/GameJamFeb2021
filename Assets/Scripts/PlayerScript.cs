95ff8a0ed07f199ad54086de036ead382ea40c98
Renem347 <renem347@gmail.com>
2021-02-06 19:17:47 +0000

    Included lean and look rotation to the player while dolly track working

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,11 +15,16 @@ public class PlayerScript : MonoBehaviour
     [Range(0, 1)]
     public float clampBoundariesMax;
 
+    public Transform aimTarget;
+
+    public int leanLimit = 20;
+    public int leanSpeed = 40;
+    public float lookSpeed = 340f;
+
     public float maxHP;
     public float currentHP;
 
     public float currentShield;
-
 
     void Start()
     {
@@ -64,13 +69,25 @@ public class PlayerScript : MonoBehaviour
     void MovementInput()
     {
         // We take of the clamp later
+
         transform.Translate(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime,
         Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0);
+        
         // Boundaries
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        pos.x = Mathf.Clamp(pos.x, 1f - clampBoundariesMax, clampBoundariesMax);
-        pos.y = Mathf.Clamp(pos.y, 1f - clampBoundariesMax, clampBoundariesMax);
+        pos.x = Mathf.Clamp(pos.x, 1 - clampBoundariesMax , clampBoundariesMax);
+        pos.y = Mathf.Clamp(pos.y, 1 - clampBoundariesMax, clampBoundariesMax);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+        
+        //Aim Position
+        aimTarget.parent.position = Vector3.zero;
+        aimTarget.localPosition = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 1);
+        transform.GetChild(0).rotation = Quaternion.RotateTowards(transform.GetChild(0).rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * lookSpeed * Time.deltaTime * 10);
+
+        // Lean Horizontal
+        Vector3 targetEulerAngels = transform.GetChild(0).localEulerAngles;
+        transform.GetChild(0).localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -Input.GetAxis("Horizontal") * leanLimit , leanSpeed));
+
     }
 
     // Update is called once per frame
